f0d1d98b06f311fa2737564c6c8aa4c670dbaf73
teru358 <terusan.358@gmail.com>
2024-07-13 22:22:20 +0900

    実行周り修正

--- modules/obs_monitor.py
+++ modules/obs_monitor.py
@@ -1,23 +1,24 @@
+# -*- coding: utf-8 -*-
 import asyncio
-import obswebsocket
+import threading
 from obswebsocket import obsws, requests, exceptions
 from .logger import LoggerConfig
 
 class OBSMonitor:
-    def __init__(self, host='localhost', port=4455, password=''):
-        self.host = host
-        self.port = port
-        self.password = password
+    def __init__(self, host:str, port:int, password:str):
         self.is_obs_running = False
         self.is_obs_streaming = False
         self.check_interval = 10
         self.logger = LoggerConfig.get_logger(self.__class__.__name__)
-        self.ws = obsws(self.host, self.port, self.password)
+        self.ws = obsws(host, port, password)
 
     async def check_obs_status(self):
         try:
             self.logger.info('Trying to connect to OBS...')
             await asyncio.wait_for(asyncio.to_thread(self.ws.connect), timeout=5)
+            self.logger.info("OBS is running.")
+            self.check_interval = 30
+            # 配信状況確認
             status = self.ws.call(requests.GetStreamStatus())
             self.is_obs_streaming = status.getOutputActive()
             self.logger.info(f"Streaming status: {self.is_obs_streaming}")
@@ -30,22 +31,34 @@ class OBSMonitor:
     async def monitor(self):
         while True:
             self.is_obs_running = await self.check_obs_status()
-            if self.is_obs_running:
-                self.logger.info("OBS is running.")
-                self.check_interval = 30
-
-            else:
+            if not self.is_obs_running:
                 self.logger.warning("OBS is not running.")
                 self.check_interval = 10
 
             await asyncio.sleep(self.check_interval)
 
     def run(self):
-        async def start():
-            while True:
-                await self.monitor()
-        asyncio.run(start())
+        print("start obs monitoring")
+        def _run():
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            loop.run_until_complete(self.monitor())
+
+        obs_monitoring_thread = threading.Thread(target=_run)
+        obs_monitoring_thread.daemon = True
+        obs_monitoring_thread.start()
+
+def main():
+    import time
+    monitor = OBSMonitor(
+        host="xxx.xxx.xxx.xxx",
+        port=4455,
+        password="your_obs_ws_pw"
+    )
+    monitor.run()
+    while True:
+        print('loop...')
+        time.sleep(2)
 
 if __name__ == "__main__":
-    monitor = OBSMonitor()
-    monitor.run()
+    main()
